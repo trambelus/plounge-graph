a6852a7896b636bc11cec71ea99b940549d60a59
Trambelus <trambelus2@gmail.com>
2015-03-28 15:29:29 -0400

    Corrected background color

--- scripts/9a71bac4.scripts.js
+++ scripts/9a71bac4.scripts.js
@@ -83,7 +83,7 @@ angular.module("cyViewerApp", ["ngCookies", "ngResource", "ngSanitize", "ngRoute
     }, a.toolbarState = {
         show: !0
     }, a.bg = {
-        color: "#FAFAFA"
+        color: "#000040"
     }, a.columnNames = [], a.edgeColumnNames = [], a.networkColumnNames = [];
     var s = {
         showOverlay: !1,
